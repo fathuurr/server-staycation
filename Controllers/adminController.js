6031a30597bb0f6d963078bfed20ab1373408c9e
Fathurrahman <nzihtlr926@gmail.com>
2020-07-05 14:45:50 +0700

    updated on CRUD

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,10 +1,34 @@
+const Category = require('../Models/Category');
+
 module.exports = {
   viewDashboard: (req, res) => {
     res.render('admin/dashboard/view_dashboard.ejs');
   },
-  viewCategory: (req, res) => {
-    res.render('admin/category/view_category.ejs');
+  viewCategory: async (req, res) => {
+    const category = await Category.find();
+    res.render('admin/category/view_category.ejs', { category });
   },
+  addCategory: async (req, res) => {
+    const { name } = req.body;
+    await Category.create({ name });
+    res.redirect('/admin/category');
+  },
+
+  editCategory: async (req, res) => {
+    const { id, name } = req.body;
+    const category = await Category.findOne({ _id: id });
+    category.name = name;
+    await category.save();
+    res.redirect('/admin/category');
+  },
+
+  deleteCategory: async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findOne({ _id: id });
+    await category.remove();
+    res.redirect('/admin/category');
+  },
+
   viewBank: (req, res) => {
     res.render('admin/bank/view_bank.ejs');
   },
